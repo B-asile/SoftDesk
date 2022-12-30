8e003fa18d85ff4020032a59e0262d35744b8526
B-asile <84376166+B-asile@users.noreply.github.com>
2022-12-30 09:20:50 +0100

    perform_create des class views.py

--- globalapp/serializers.py
+++ globalapp/serializers.py
@@ -3,27 +3,32 @@ from .models import Projects, Contributors, Issues, Comments
 
 
 class ProjectsSerializers(serializers.ModelSerializer):
-
     class Meta:
         model = Projects
         fields = "__all__"
 
-class ContributorsSerializers(serializers.ModelSerializer):
+        extra_kwargs = {'author_user_id': {'read_only': True},
+                        'project_id': {'read_only':True}
+                        }
 
+
+class ContributorsSerializers(serializers.ModelSerializer):
     class Meta:
         model = Contributors
         fields = "__all__"
 
 
 class IssuesSerializers(serializers.ModelSerializer):
-
     class Meta:
         model = Issues
         fields = "__all__"
 
+        extra_kwargs = {'author_user_id': {'read_only': True},
+                        'project_id': {'read_only': True}
+                        }
+
 
 class CommentsSerializers(serializers.ModelSerializer):
-
     class Meta:
         model = Comments
         fields = "__all__"
--- globalapp/views.py
+++ globalapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Projects, Contributors, Issues, Comments
@@ -12,6 +13,15 @@ class ProjectApiView(viewsets.ModelViewSet):
     serializer_class = ProjectsSerializers
     queryset = Projects.objects.all()
     permission_classes = (IsAuthenticated, )
+
+    def perform_create(self, serializer):
+        '''create author_User par default'''
+        new_project = serializer.save(author_user_id=self.request.user)
+        Contributors.objects.create(user_id=self.request.user, project_id=new_project, permission='author', role='project_owner')
+        new_project.contribs.add(self.request.user)
+        new_project.save()
+
+
 
 
 class ContributorApiView(viewsets.ModelViewSet):
@@ -27,9 +37,21 @@ class IssueApiView(viewsets.ModelViewSet):
     queryset = Issues.objects.all()
     permission_classes = (IsAuthenticated,)
 
+    def perform_create(self, serializer):
+        new_issue = serializer.save(author_user_id=self.request.user,
+                                    project_id=get_object_or_404(Projects, pk=self.kwargs['projects_pk'])
+                                    )
+        new_issue.save()
+
 
 class CommentApiView(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = CommentsSerializers
     queryset = Comments.objects.all()
     permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        new_comment = serializer.save(author_user_id=self.request.user,
+                                    issue_id=get_object_or_404(Issues, pk=self.kwargs['issues_pk'])
+                                    )
+        new_comment.save()
